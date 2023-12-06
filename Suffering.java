c91a6339e56c27375f8bf2e3316441f99f415705
Mindy Medeiros <mmedei6@wgu.edu>
2023-12-05 21:40:50 -0500

    Beginning the suffering.

---
+++ Suffering.java
@@ -0,0 +1,19 @@
+class Suffering {
+    public static void main(String[] args) {
+        System.out.println("AAAAAAAAAAAAAAHHHHHHHHHHHHHHHHHHHHHHHHH!!!\nANYTHING BUT JAVA!!!");
+        System.out.println("\nWell, except maybe Rust. ...");
+    }
+
+    public static double calcPizzaArea (double pizzaDiameter) {
+        double pizzaRadius;
+        double pizzaArea;
+        double piVal = 3.14159265;
+
+        pizzaRadius = pizzaDiameter / 2.0;
+        pizzaArea = piVal * pizzaRadius * pizzaRadius;
+        return pizzaArea;
+    }
+
+       
+
+}
