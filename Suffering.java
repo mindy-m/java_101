22e8f8b6973d817e6686ce836a6f282050424fe0
Mindy Medeiros <mmedei6@wgu.edu>
2023-12-05 23:09:30 -0500

    Made text output more pleasant to look at.

--- Suffering.java
+++ Suffering.java
@@ -1,19 +1,19 @@
 class Suffering {
     // Function signature
     public static void main(String[] args) {
-        System.out.println("AAAAAAAAAAAAAAHHHHHHHHHHHHHHHHHHHHHHHHH!!!\nANYTHING BUT JAVA!!!");
-        System.out.println("\nWell, except maybe Rust. ...\n");
+        System.out.println("\nAAAAAAAAAAAAAAHHHHHHHHHHHHHHHHHHHHHHHHH!!!    ANYTHING but JAVA!!!");
+        System.out.println("...Well, except maybe Rust...\n\n");
         
         double inchesOfPizzaArea = calcPizzaArea(12);
-        System.out.println("How many inches of pizza will I have?");
+        System.out.println("Given a '12 inch' pizza, how many inches of pizza will I have?");
         System.out.println(inchesOfPizzaArea);
 
         Goat orlando = new Goat("Orlando", 99, true);
-        System.out.println("\nHere's some info about Orlando (who happens to be a goat): ");
+        System.out.println("\n\nHere's some info about Orlando (who happens to be a goat): ");
         System.out.println(orlando);
 
         Goat lily = new Goat("Lily", 12, false);
-        System.out.println("\n\n...and here's some info about Lily! (also a goat)");
+        System.out.println("\n...and here's some info about Lily! (also a goat)");
         System.out.println(lily);
     }
 
