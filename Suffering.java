601b484efc3a978c97c03ddaf2db09f693f47e08
Mindy Medeiros <mmedei6@wgu.edu>
2023-12-05 21:50:15 -0500

    Print a sample pizza area given a 12 inch pizza.

--- Suffering.java
+++ Suffering.java
@@ -1,9 +1,13 @@
 class Suffering {
+    // Function signature
     public static void main(String[] args) {
         System.out.println("AAAAAAAAAAAAAAHHHHHHHHHHHHHHHHHHHHHHHHH!!!\nANYTHING BUT JAVA!!!");
-        System.out.println("\nWell, except maybe Rust. ...");
+        System.out.println("\nWell, except maybe Rust. ...\n");
+        double inchesOfPizzaArea = calcPizzaArea(12);
+        System.out.println(inchesOfPizzaArea);
     }
 
+    // Will return a double precision float (a number data type)
     public static double calcPizzaArea (double pizzaDiameter) {
         double pizzaRadius;
         double pizzaArea;
