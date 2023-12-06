71a29bfaef2e3a965c0f411a93bb6a74f6007763
Mindy Medeiros <mmedei6@wgu.edu>
2023-12-05 22:12:32 -0500

    Set up constructor

--- Goat.java
+++ Goat.java
@@ -2,4 +2,15 @@ public class Goat {
     public String name;
     public double powerLevel;
     public boolean isGrumpy;
+
+    // flashback to constructors, save me
+    public Goat(
+            String name,
+            double powerLevel,
+            // no dangling commas in Java
+            boolean isGrumpy) {
+                this.name = name;
+                this.powerLevel = powerLevel;
+                this.isGrumpy = isGrumpy;
+    }
 }
